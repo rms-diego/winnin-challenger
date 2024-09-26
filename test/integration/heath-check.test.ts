b0f8aae29fbbaa45b7cd3f6deac1316f7f164b00
Diego Ramos <diegorbrito9@gmail.com>
2024-09-26 11:39:45 -0300

    fix: update health check integration test

--- test/integration/heath-check.test.ts
+++ test/integration/heath-check.test.ts
@@ -11,9 +11,14 @@ describe("GET / - Health check test cases", () => {
 
   test("it should be able to get health check message", async () => {
     const { body } = await request.get("/").expect(200);
-    expect(body).toEqual({
-      message: "server is running",
-      documentation: "/docs",
-    });
+
+    expect(body.message).toBeDefined();
+    expect(body.message).toBe("server is running");
+
+    expect(body.documentation).toBeDefined();
+    expect(body.documentation).toBe("/docs");
+
+    expect(body.instanceId).toBeDefined();
+    expect(typeof body.instanceId).toBe("string");
   });
 });
