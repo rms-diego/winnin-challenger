0e58a137ac1e3af4564f4bd398f38029c358c526
Diego Ramos <diegorbrito9@gmail.com>
2024-09-25 22:43:12 -0300

    chore: change cron job

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,8 @@ async function main() {
   await app.listen({ host: "0.0.0.0", port: env.PORT });
 
   // cron job
-  cron.schedule("*/10 * * * *", () => redditService.fetchPostsFromReddit());
-  // cron.schedule("0 0 * * *", () => redditService.fetchPostsFromReddit());
+  // cron.schedule("*/10 * * * *", () => redditService.fetchPostsFromReddit());
+  cron.schedule("0 0 * * *", () => redditService.fetchPostsFromReddit());
 }
 
 main();
